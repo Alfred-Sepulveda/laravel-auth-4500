df3270aaccea2083652813c8184736cff03551b6
Alfred Sepulveda <006372934@coyote.csusb.edu>
2022-05-02 15:53:34 -0700

    added https

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+if (env('APP_ENV') ==='local'){
+    URL::forceScheme('https');
+}
